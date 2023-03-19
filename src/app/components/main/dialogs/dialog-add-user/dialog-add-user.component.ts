04fec5cefd813a70e50cfdb7e3e2b250b747d85e
Sneakz96 <Jahleelr-h@gmx.de>
2023-03-19 14:17:48 +0100

    edit add task alerts

--- src/app/components/main/dialogs/dialog-add-user/dialog-add-user.component.ts
+++ src/app/components/main/dialogs/dialog-add-user/dialog-add-user.component.ts
@@ -28,7 +28,7 @@ export class DialogAddUserComponent implements OnInit {
     public data: DataService,
     public dialogRef: MatDialogRef<DialogAddUserComponent>
   ) {
-   
+
   }
 
   // 
@@ -40,121 +40,106 @@ export class DialogAddUserComponent implements OnInit {
   // SET FORM OF NEW CONTACT
   setUserForm() {
     this.contactForm = new FormGroup({
-    'firstName': new FormControl('', [Validators.required, Validators.minLength(5)], []),
-    'lastName': new FormControl(this.user.lastName, [Validators.required, Validators.minLength(5)], []),
-    'email': new FormControl(this.user.email, [Validators.required, Validators.email], []),
-    'phone': new FormControl(this.user.phone, [Validators.required], []),
-  });
+      'firstName': new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z]+$/)]),
+      'lastName': new FormControl(this.user.lastName, [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z]+$/)]),
+      'email': new FormControl(this.user.email, [Validators.required, Validators.email], []),
+      'phone': new FormControl(this.user.phone, [Validators.required, Validators.pattern(/^\+?\d{6,}$/)]),
+    });
 
-}
-
-// CHECK SENDED USER FORM (ADD USER)
-checkForm() {
-  let { firstName, lastName, email, phone } = this.contactForm.value;
-  console.log(this.contactForm)
-  console.log(firstName, lastName, email, phone)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // let formattedFirstName = this.data.capitalizeFirstLetter(firstName.trim());
-  // let formattedLastName = this.data.capitalizeFirstLetter(lastName.trim());
-  // let isFirstNameValid = this.data.isValidInput(formattedFirstName) && formattedFirstName.length >= 3;
-  // let isLastNameValid = this.data.isValidInput(formattedLastName) && formattedLastName.length >= 3;
-  // let isEmailValid = this.data.isValidEmail(email.trim());
-  // let isNumberValid = this.data.isValidNumber(phone.trim());
-
-  // if (!isFirstNameValid || !isLastNameValid || !isEmailValid || !isNumberValid) {
-  //   console.error('Error: Invalid input');
-  //   return;
-  // }
-  // this.user.firstName = formattedFirstName;
-  // this.user.lastName = formattedLastName;
-  // this.user.email = email.trim();
-  // this.user.phone = phone.trim();
-  // this.setUserID();
-  // this.setColor();
-  // console.log(this.user);
-  // this.addUser();
-  // this.contactForm.reset();
-}
-
-// SHOULD CLEAR VALUES OF DIALOG
-clearValues() {
-  this.contactForm.reset();
-}
-
-// SAVE NEW USER TO DB
-saveUserToFirestore() {
-  let coll = collection(this.firestore, 'allContacts');
-  setDoc(doc(coll), this.user.toJSON());
-}
-
-// CLOSE DIALOG TO CREATE NEW USER
-closeDialog() {
-  this.dialogRef.close();
-}
-
-// GIVE NEW USER RANDOM ID
-setUserID() {
-  this.user.id = 20000 * Math.random();
-}
-
-// SET BG_COLOR OF CIRCLE BY FIRST LETTER OF LAST NAME
-setColor() {
-  switch (this.user.lastName.charCodeAt(0) % 6) {
-    case 0:
-      this.user.color = 'lightgreen'
-      break;
-    case 1:
-      this.user.color = 'lightgrey'
-      break;
-    case 2:
-      this.user.color = 'lightblue'
-      break;
-    case 3:
-      this.user.color = 'rgb(203, 87, 87)'
-      break;
-    case 4:
-      this.user.color = '#d0d046'//YELLOW
-      break;
-    case 5:
-      this.user.color = 'orange'
-      break;
-    case 6:
-      this.user.color = 'purple'
-      break;
-    case 7:
-      this.user.color = 'pink'
-      break;
-    default:
   }
+
+  // CHECK SENDED USER FORM (ADD USER)
+  checkForm() {
+    let { firstName, lastName, email, phone } = this.contactForm.value;
+    let formattedFirstName = this.data.capitalizeFirstLetter(firstName.trim());
+    let formattedLastName = this.data.capitalizeFirstLetter(lastName.trim());
+    let isFirstNameValid = this.data.isValidInput(formattedFirstName) && formattedFirstName.length >= 3;
+    let isLastNameValid = this.data.isValidInput(formattedLastName) && formattedLastName.length >= 3;
+    let isEmailValid = this.data.isValidEmail(email.trim());
+    let isNumberValid = this.data.isValidNumber(phone.trim());
+
+    if (!isFirstNameValid || !isLastNameValid || !isEmailValid || !isNumberValid) {
+      // alert('Error: Invalid input');
+      // FORMALERT HERE?
+
+      return;
+    } else if(this.contactForm.valid){
+
+      this.user.firstName = formattedFirstName;
+      this.user.lastName = formattedLastName;
+      this.user.email = email.trim();
+      this.user.phone = phone.trim();
+      this.setUserID();
+      this.setColor();
+      // this.addUser();
+      this.contactForm.reset();
+    }
 }
 
-// CHECK FORM VALIDATION AND ADD CREATED USER TO CONTACT-LIST
-addUser() {
-  debugger;
-  this.data.contactCreated = true;
-  this.data.allContacts = [];
-  this.data.loadContacts();
-  this.saveUserToFirestore();
-  this.clearValues();
-  this.closeDialog();
-  this.router.navigate(['/kanbanboard/contacts']);
-  setTimeout(() => {
-    this.data.contactCreated = false;
-  }, 3000);
-}
+  // SHOULD CLEAR VALUES OF DIALOG
+  clearValues() {
+    this.contactForm.reset();
+  }
+
+  // SAVE NEW USER TO DB
+  saveUserToFirestore() {
+    let coll = collection(this.firestore, 'allContacts');
+    setDoc(doc(coll), this.user.toJSON());
+  }
+
+  // CLOSE DIALOG TO CREATE NEW USER
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  // GIVE NEW USER RANDOM ID
+  setUserID() {
+    this.user.id = 20000 * Math.random();
+  }
+
+  // SET BG_COLOR OF CIRCLE BY FIRST LETTER OF LAST NAME
+  setColor() {
+    switch (this.user.lastName.charCodeAt(0) % 6) {
+      case 0:
+        this.user.color = 'lightgreen'
+        break;
+      case 1:
+        this.user.color = 'lightgrey'
+        break;
+      case 2:
+        this.user.color = 'lightblue'
+        break;
+      case 3:
+        this.user.color = 'rgb(203, 87, 87)'
+        break;
+      case 4:
+        this.user.color = '#d0d046'//YELLOW
+        break;
+      case 5:
+        this.user.color = 'orange'
+        break;
+      case 6:
+        this.user.color = 'purple'
+        break;
+      case 7:
+        this.user.color = 'pink'
+        break;
+      default:
+    }
+  }
+
+  // CHECK FORM VALIDATION AND ADD CREATED USER TO CONTACT-LIST
+  addUser() {
+    debugger;
+    this.data.contactCreated = true;
+    this.data.allContacts = [];
+    this.data.loadContacts();
+    this.saveUserToFirestore();
+    this.clearValues();
+    this.closeDialog();
+    this.router.navigate(['/kanbanboard/contacts']);
+    setTimeout(() => {
+      this.data.contactCreated = false;
+    }, 3000);
+  }
 }
